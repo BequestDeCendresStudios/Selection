d715955b0175496ae8aa398458319722ff7d82c2
Sarah Weaver <9245433+LWFlouisa@users.noreply.github.com>
2025-09-05 19:19:16 +0000

    Update evaluate.rb

--- evaluate.rb
+++ evaluate.rb
@@ -6,12 +6,6 @@ def learn_about_animals
                  :gerbil, "does not use a hamster wheel.") # 0.054450000000000005
 end
 
-def learn_about_death
-  get_statistics(:guillotines,            "had once been considered a swift death.", # 0.054450000000000005
-                 :nooses,      "can be quick but often victims strangled to death.", # 0.054450000000000005
-                 :garrotes,     "often left victims suffocating, or neck breaking.") # 0.054450000000000005
-end
-
 def reconsider_assertions
   reconsider
 end
@@ -20,5 +14,5 @@ def reassess_assertions
   reasses
 end
 
-learn_about_death
+learn_about_animals
 3.times do reassess_assertions end
